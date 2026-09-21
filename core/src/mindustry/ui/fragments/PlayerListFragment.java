ad648779a277fd49ce3337342b221bab98cf0b0b
Yaaty256 <yaaty256@gmail.com>
2026-09-21 06:03:12 +0300

    h

--- core/src/mindustry/ui/fragments/PlayerListFragment.java
+++ core/src/mindustry/ui/fragments/PlayerListFragment.java
@@ -399,7 +399,7 @@ public class PlayerListFragment{
                         ).tooltip("@client.modmute").get();
                         muteButton.update(() -> {
                             muteButton.setDisabled(!user.serverModerationStateKnown);
-                            muteButton.getStyle().imageUpColor =!user.serverMuted ? Color.valueOf("#222222") : Color.gray;
+                            muteButton.getStyle().imageUpColor = user.serverMuted ? Color.valueOf("#222222") : Color.gray;
                         });
                     }
                 }
